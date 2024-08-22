51ede6aa3e8b989299f213d2eb261a9ebf4e03dc
vai4snow <vincent.airault@devoteam.com>
2024-08-22 15:58:10 +0200

    Update streamlit_app.py
    add subheader

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,11 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        # st.text(fruityvice_response.json()
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -47,8 +52,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json()
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
